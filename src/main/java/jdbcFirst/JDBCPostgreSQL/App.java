eb3239830caaad839c6fa8d4962310f44d7ff412
Artem Yuran <yuoran371@gmail.com>
2023-11-24 16:07:31 +0300

    Add new project which realizing JDBC and PostgreSQL

---
+++ src/main/java/jdbcFirst/JDBCPostgreSQL/App.java
@@ -0,0 +1,13 @@
+package jdbcFirst.JDBCPostgreSQL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
