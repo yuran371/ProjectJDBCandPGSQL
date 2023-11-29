8923ea8fe436c44954b34695fb51ace8701473c0
Artem Yuran <yuoran371@gmail.com>
2023-11-29 16:27:44 +0300

    Add the function to batch requests

---
+++ src/main/java/jdbcFirst/util/BatchTransactionRunner.java
@@ -0,0 +1,43 @@
+package jdbcFirst.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BatchTransactionRunner {
+	public static void main(String[] args) throws SQLException {
+		Long id = 8l;
+		String sqlTicket = """
+				DELETE FROM ticket
+				WHERE flight_id = 
+				""" + id;
+		String sqlFlight = """
+				DELETE FROM flight
+				WHERE id = 
+				""" + id;
+		Connection connection = null;
+		Statement statement = null;
+		
+		try {
+			connection = ConnectionManager.open();
+			
+			connection.setAutoCommit(false);
+			
+			statement = connection.createStatement();
+			
+			statement.addBatch(sqlTicket);
+			statement.addBatch(sqlFlight);
+			
+			statement.executeBatch();
+			
+			connection.commit();
+		} catch (Exception e) {
+			if(connection!=null) {
+				connection.rollback();
+			}
+		} finally {
+			connection.close();
+			statement.close();
+		}
+	}
+}
