2f921d6ea8e411ed82003c96d948aa0ad84ad1c0
Artem Yuran <yuoran371@gmail.com>
2023-11-30 15:38:12 +0300

    Add Connection pool (006 Пул соединений)

--- src/main/java/jdbcFirst/util/ConnectionManager.java
+++ src/main/java/jdbcFirst/util/ConnectionManager.java
@@ -1,8 +1,15 @@
 package jdbcFirst.util;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 /*
  * 003 Подключение к базам данных
@@ -13,15 +20,45 @@ public final class ConnectionManager {
 	private static final String PASSWORD_KEY = "db.password";
 	private static final String USERNAME_KEY = "db.username";
 	private static final String URL_KEY = "db.url";
+	private static final String POOL_SIZE_KEY= "db.pool.size";
+	private static Integer DEFAULT_POOL_SIZE = 10;
+	private static BlockingQueue<Connection> pool;		// 006 Пул соединений (JDBC Аdvanced); потокобезопасная коллекция
+	private static List<Connection> sourceConnections;
 
 	static {
 		loadDriver(); 
+		initConnectionPool();  // 006 Пул соединений (JDBC Аdvanced)
 	}
 	
 	private ConnectionManager() {
 	}
 	
-	public static Connection open() {
+	// 006 Пул соединений (JDBC Аdvanced)
+	private static void initConnectionPool() {
+		String poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
+		int size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+		pool = new ArrayBlockingQueue<>(size);
+		sourceConnections = new ArrayList<>(size);
+		for (int i=0; i<size; i++) {
+			Connection connection = open();
+			Connection proxyConnection = (Connection) Proxy.newProxyInstance(ConnectionManager.class.getClassLoader(), new Class[] {Connection.class}, 
+					(proxy, method, args) -> method.getName().equals("close")
+						? pool.add((Connection) proxy)
+					: method.invoke(connection, args));
+			pool.add(proxyConnection);
+			sourceConnections.add(connection);	// Чтобы вызвать именно метод close (не перезаписанный в proxyConnection
+		}
+	}
+	
+	public static Connection get() {
+		try {
+			return pool.take();
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Connection open() {
 		try {
 			return DriverManager.getConnection(
 					PropertiesUtil.get(URL_KEY),
@@ -38,6 +75,16 @@ public final class ConnectionManager {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
+		}
+	}
+	
+	public static void closePool() {
+		for (Connection sourceConnection : sourceConnections) {
+			try {
+				sourceConnection.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 }
