ce41a781bd1eac24364e829f450c4366f2b313dc
Artem Yuran <yuoran371@gmail.com>
2023-12-01 15:32:50 +0300

    Add DAO UPDATE and SELECT

--- src/main/java/jdbcFirst/DaoRunner.java
+++ src/main/java/jdbcFirst/DaoRunner.java
@@ -1,5 +1,9 @@
 package jdbcFirst;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 import jdbcFirst.dao.TicketDao;
 import jdbcFirst.entity.Ticket;
 
@@ -11,14 +15,25 @@ import jdbcFirst.entity.Ticket;
 public class DaoRunner {
 	public static void main(String[] args) {
 		TicketDao ticketDao = TicketDao.getInstance();
-		Ticket ticket = new Ticket();
-		ticket.setPassengerNo("wdgweg");
-		ticket.setPassengerName("qweewq");
-		ticket.setFlightId(5l);
-		ticket.setSeatNo("13A");
-		ticket.setCost(1300L);
-//		Ticket createTicket = ticketDao.create(ticket);
-		boolean delete = ticketDao.delete(60l);
-		System.out.println(delete);
+//		Ticket ticket = new Ticket();
+//		ticket.setPassengerNo("wdgweg");
+//		ticket.setPassengerName("qweewq");
+//		ticket.setFlightId(5l);
+//		ticket.setSeatNo("13A");
+//		ticket.setCost(1300L);
+////		Ticket createTicket = ticketDao.create(ticket);
+////		System.out.println(createTicket);
+////		boolean delete = ticketDao.delete(60l);
+////		System.out.println(delete);
+//		Optional<Ticket> findById = ticketDao.findById(2l);
+//		System.out.println(findById);
+		
+//		Optional<Ticket> MaybeTicket = ticketDao.findById(2l);
+//		MaybeTicket.ifPresent(ticketx -> {
+//			ticketx.setCost(188l);
+//			ticketDao.update(ticketx);
+//		});
+		List<Ticket> findAll = ticketDao.findAll();
+		System.out.println(findAll);
 	}
 }
--- src/main/java/jdbcFirst/dao/TicketDao.java
+++ src/main/java/jdbcFirst/dao/TicketDao.java
@@ -1,10 +1,13 @@
 package jdbcFirst.dao;
 
-import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import jdbcFirst.entity.Ticket;
 import jdbcFirst.exception.DaoException;
@@ -29,6 +32,31 @@ public class TicketDao {
 	private static final String CREATE_SQL = """
 			INSERT INTO ticket (passenger_no, passenger_name, flight_id, seat_no, cost)
 			VALUES (?, ?, ?, ?, ?);
+			""";
+	
+	private static final String UPDATE_SQL = """
+			UPDATE ticket 
+			SET passenger_no = ?,
+				passenger_name = ?,
+				flight_id = ?,
+				seat_no = ?,
+				cost = ?
+			WHERE id = ?;
+			""";
+	
+	private static final String FIND_ALL_SQL = """
+			SELECT id, 
+				passenger_no, 
+				passenger_name, 
+				flight_id, 
+				seat_no, 
+				cost
+			FROM ticket
+			""";
+	
+	private static final String FIND_BY_ID_SQL = FIND_ALL_SQL + 
+			"""
+			WHERE id = ?
 			""";
 	
 	private TicketDao() {
@@ -64,6 +92,71 @@ public class TicketDao {
 		} catch (SQLException e) {
 			throw new DaoException(e);
 		}
+	}
+	
+	public void update(Ticket ticket) {
+		try (Connection connection = ConnectionManager.get();
+			PreparedStatement prepareStatement = connection.prepareStatement(UPDATE_SQL)) {
+			prepareStatement.setString(1, ticket.getPassengerNo());
+			prepareStatement.setString(2, ticket.getPassengerName());
+			prepareStatement.setLong(3, ticket.getFlightId());
+			prepareStatement.setString(4, ticket.getSeatNo());
+			prepareStatement.setLong(5, ticket.getCost());
+			prepareStatement.setLong(6, ticket.getId());
+			
+			
+			prepareStatement.executeUpdate();
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		}
+	}
+	
+	// 004 DAO. Операции UPDATE и SELECT
+	public Optional<Ticket> findById(Long id) {
+		try (Connection connection = ConnectionManager.get();
+			PreparedStatement prepareStatement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+			prepareStatement.setLong(1, id);
+			
+			ResultSet resultSet = prepareStatement.executeQuery();
+			Ticket ticket = null;
+			if (resultSet.next()) {
+				ticket = new Ticket(
+						resultSet.getLong("id"),
+						resultSet.getString("passenger_no"),
+						resultSet.getString("passenger_name"),
+						resultSet.getLong("flight_id"),
+						resultSet.getString("seat_no"),
+						resultSet.getLong("cost")
+				);
+			}
+			
+			return Optional.ofNullable(ticket);
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		}
+	}
+	
+	// 004 DAO. Операции UPDATE и SELECT
+	public List<Ticket> findAll() {
+		try (Connection connection = ConnectionManager.get();
+			PreparedStatement prepareStatement = connection.prepareStatement(FIND_ALL_SQL)) {
+			ResultSet resultSet = prepareStatement.executeQuery();
+			List<Ticket> tickets = new ArrayList<>();
+			while (resultSet.next()) {
+				tickets.add(new Ticket(
+						resultSet.getLong("id"),
+						resultSet.getString("passenger_no"),
+						resultSet.getString("passenger_name"),
+						resultSet.getLong("flight_id"),
+						resultSet.getString("seat_no"),
+						resultSet.getLong("cost")
+				));
+			}
+			return tickets;
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		}
+		
 	}
 	
 	public static TicketDao getInstance() {
